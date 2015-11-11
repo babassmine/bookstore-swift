0e4e0b8d82909c4ec1cfbb1bf58e138cc831a042
Abass Sesay <bassco10@gmail.com>
2015-11-10 22:06:26 -0500

    add a class for author

---
+++ Author.swift
@@ -0,0 +1,15 @@
+//
+//  Author.swift
+//  BookStore
+//
+//  Created by Abass Sesay on 11/10/15.
+//  Copyright © 2015 Abass Sesay. All rights reserved.
+//
+
+import Foundation
+
+class Author {
+    var name =  ""
+    var pub_house = ""
+    var books = [ "params", "params"]
+}
