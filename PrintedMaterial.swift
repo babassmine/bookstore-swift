0b0197da8d94f0a435db57b05b1ca0b3ac89cf09
Abass Sesay <bassco10@gmail.com>
2015-11-10 22:09:23 -0500

    create a PrintedMaterial class

---
+++ PrintedMaterial.swift
@@ -0,0 +1,15 @@
+//
+//  PrintedMaterial.swift
+//  BookStore
+//
+//  Created by Abass Sesay on 11/10/15.
+//  Copyright © 2015 Abass Sesay. All rights reserved.
+//
+
+import Foundation
+
+class PrintedMaterial {
+    var name = ""
+    var publisher = ""
+    var ISBN = ""
+}
